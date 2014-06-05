cb54e52dd3ca494a78043b6d3d49260a2299b70d
Chris Zubak-Skees <chriszs@gmail.com>
2014-06-05 18:32:37 -0400

    Adapted for CPI

--- www/js/app.js
+++ www/js/app.js
@@ -135,7 +135,7 @@ $(function(){
     $theme_buttons.on('click', function(){
         $theme_buttons.removeClass().addClass('btn btn-default');
         $(this).addClass('btn-primary');
-        $poster.removeClass('poster-news poster-music poster-fresh-air poster-snap-judgement')
+        $poster.removeClass('poster-light poster-dark')
                     .addClass('poster-' + $(this).attr('id'));
     });
 
